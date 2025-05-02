163ddb1f38a11d3e824fac0a3e4654b0dbacdb4a
Ayman El Amrani <ayman.elamrani99@gmail.com>
2025-05-02 11:14:26 +0100

    Initial Commit

---
+++ NFPayTests/NFPayTests.swift
@@ -0,0 +1,17 @@
+//
+//  NFPayTests.swift
+//  NFPayTests
+//
+//  Created by Ayman El Amrani on 02/05/2025.
+//
+
+import Testing
+@testable import NFPay
+
+struct NFPayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
